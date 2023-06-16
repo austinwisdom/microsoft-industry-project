3aef9d9f49e4412ffac0b2864314801bbf2b5562
Austin <ataraxia@Austins-MacBook-Air.local>
2023-06-16 12:49:33 -0300

    Fixed spacing.

--- client/src/components/Header/Header.js
+++ client/src/components/Header/Header.js
@@ -8,12 +8,13 @@ const Header = () => {
     return (
         <div>
             <nav className="nav">
-                <div className="nav__div--logo">
+                <div className="nav__div--main">
+                    <div className="nav__div--logo">
                     <img src="" />
-                    <NavLink><h3>Microsoft Account |</h3></NavLink>
+                    <NavLink><h3 className="nav__div--link">Microsoft Account |</h3></NavLink>
+                    <NavLink><h4>Your Information</h4></NavLink>
                 </div>
                 <div className="nav__div--navbar">
-                    <NavLink><h4 className="">Your Information</h4></NavLink>
                     <ul className="nav__ul">
                         <NavLink><li>Privacy</li></NavLink>
                         <NavLink><li>Security</li></NavLink>
@@ -22,8 +23,9 @@ const Header = () => {
                         <NavLink><li>Devices</li></NavLink>
                     </ul>
                 </div>
+                </div>
                 <div>
-                    <img src={questionIcon} />
+                    <img className="nav__icon" src={questionIcon} />
                 </div>
             </nav>
         </div>

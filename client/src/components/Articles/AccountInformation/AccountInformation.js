04cc8ea1e634966d30d4111e59f268d78f35d49b
Alina <allianpi@gmail.com>
2023-06-16 12:45:38 -0400

    add your info head component

--- client/src/components/Articles/AccountInformation/AccountInformation.js
+++ client/src/components/Articles/AccountInformation/AccountInformation.js
@@ -1,9 +1,18 @@
 import "./AccountInformation.scss";
+import keyIcon from "../../../assets/key-i.png"
 
 const AccountInformation = () => {
   return (
-    <>
-    </>
+    <section className="account-info"> 
+      <h2>Your information</h2> 
+      <div className="account-info__password">
+        <img className="account-info__key-i" src={keyIcon}/>
+        <div >
+          <p>Change password</p>
+          <p>Security</p>
+        </div>
+      </div>
+    </section>
   )
 }
 
